83a09c7a8caf11222d51d99a3257e5043312c2f0
cleoc <kimuraam@mail.uc.edu>
2022-09-12 21:34:39 -0400

    project init

--- src/evenOdd.java
+++ src/evenOdd.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class evenOdd {
+public class evenOdd {
+    public static void main(String []args){
+        int input = 7;
+        System.out.println("Enter a number");
+        if(input % 2 ==0)
+            System.out.println(input + " is even.");
+        else
+            System.out.println(input + " is odd.");
+    }
 }
